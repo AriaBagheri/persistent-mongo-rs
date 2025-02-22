50aa8a5f23dada78644eb0c838029c0a47d35395
Aria Bagheri <ariab9342@gmail.com>
2025-02-22 03:13:54 +0000

    removed excessive from_str

--- src/mongo_uri.rs
+++ src/mongo_uri.rs
@@ -2,12 +2,6 @@ use std::str::FromStr;
 
 pub struct MongoUri(String);
 
-impl MongoUri {
-    pub fn from_str(value: String) -> MongoUri {
-        MongoUri(value)
-    }
-}
-
 impl FromStr for MongoUri {
     type Err = &'static str;
 
